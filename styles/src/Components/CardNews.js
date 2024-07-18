579ebe4d45e4c1714ac09a7144431141aef2a420
azevdoDev <contato.azevedo@outlook.com.br>
2024-07-18 02:11:21 -0300

    resolvido

--- styles/src/Components/CardNews.js
+++ styles/src/Components/CardNews.js
@@ -1,6 +1,7 @@
 class CardNews extends HTMLElement {
     constructor() {
         super();
+        console.log("CardNews constructor chamado");
 
         const shadow = this.attachShadow({ mode: "open" });
         shadow.innerHTML = "<h1>Matheus Azevedo</h1>";
